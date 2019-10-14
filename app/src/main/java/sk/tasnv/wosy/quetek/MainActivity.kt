04b73026d8810124a10b8c8354b39fe9a05c1193
markotomcik <markotomcik@gmail.com>
2019-10-14 16:08:09 +0200

    List

--- app/src/main/java/sk/tasnv/wosy/quetek/MainActivity.kt
+++ app/src/main/java/sk/tasnv/wosy/quetek/MainActivity.kt
@@ -2,17 +2,24 @@ package sk.tasnv.wosy.quetek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentAdapter = PagerAdapter(supportFragmentManager)
-        viewPager.adapter = fragmentAdapter
+        listView = findViewById<ListView>(R.id.list_view)
 
+        val moduleTitles = arrayOf("Rozvrh", "Hodiny", "Chat", "Bugy")
 
+        // Adaptér pre ListView
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, moduleTitles)
+        listView.adapter = adapter
     }
 }
